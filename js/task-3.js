fd91fd67db28c4dbf982b34bf670cf325fec1575
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-05-19 15:38:28 +0200

    add log

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,13 @@
 
 
 function getElementWidth(content, padding, boarder) {
-    const contentNumber = Number.parseInt(content);
-    const paddingNumber = Number.parseInt(padding);
-    const boarderNumber = Number.parseInt(boarder);
+    const contentNumber = Number.parseFloat(content);
+    const paddingNumber = Number.parseFloat(padding);
+    const boarderNumber = Number.parseFloat(boarder);
     return contentNumber + (paddingNumber * 2) + (boarderNumber * 2);
 }
+
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
